e09ec453953ea6c732e33046c1e0047bf09ec9b3
rianneam03 <rianneam03@gmail.com>
2025-11-18 22:29:35 -0600

    Move render-build.sh to project root

--- backend/scholartrack/settings.py
+++ backend/scholartrack/settings.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "corsheaders",        # ✅ must be at the top
     "rest_framework",
-    "core",               # your app
+    "backend.core",               # your app
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
