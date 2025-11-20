cb912ad2e3ccc66b9510da5609b84dc25fda4707
rianneam03 <rianneam03@gmail.com>
2025-11-19 22:15:22 -0600

    Fix import paths for Render

--- backend/scholartrack/urls.py
+++ backend/scholartrack/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # 👈 Connects your app
+    path('', include('backend.core.urls')),  # 👈 Connects your app
 ]
