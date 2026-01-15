3bae14d401c4812a001bf60a60305259914a33a8
rianneam03 <rianneam03@gmail.com>
2026-01-15 13:43:52 -0600

    Update color scheme

--- backend/frontend/src/components/Login.js
+++ backend/frontend/src/components/Login.js
@@ -65,10 +65,10 @@ function Login() {
           type="submit"
           style={{
             padding: "12px 40px",
-            background: "#2e8b57",
+            //background: "#2e8b57",
             color: "white",
             border: "none",
-            fontSize: 16,
+            //fontSize: 16,
           }}
         >
           LOGIN NOW
