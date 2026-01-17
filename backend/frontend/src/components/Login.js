db475ac55bfec4de7f5c6fbdb6e441340c31ec1c
rianneam03 <rianneam03@gmail.com>
2026-01-16 23:03:40 -0600

    Formatting fix

--- backend/frontend/src/components/Login.js
+++ backend/frontend/src/components/Login.js
@@ -66,10 +66,10 @@ function Login() {
         </button>
       </form>
 
-      <p className={`login-message ${message.includes("SUCCESS") ? "success" : "error"}`}>
+      <p className={`login-message. ${message.includes("SUCCESS") ? "success" : "error"}`}>
         {message}
       </p>
-      
+
     </div>
   );
 }
