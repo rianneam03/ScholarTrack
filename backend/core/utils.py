2583cf9f08aefc4b5443133743bbf54e0b1c8dfb
rianneam03 <rianneam03@gmail.com>
2026-01-27 02:08:21 -0600

    Activation Email

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,15 +1,27 @@
-# Email Function
-from django.core.mail import send_mail
-from django.conf import settings
+# utils.py
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 def send_activation_email(to_email, token):
-    activation_link = f"https://yourfrontend.com/activate?token={token}"
-    subject = "Activate your ScholarTrack account"
-    message = f"Hello!\n\nClick the link below to set your password and activate your account:\n\n{activation_link}\n\nIf you didn't request this, ignore this email."
-    send_mail(
-        subject,
-        message,
-        settings.DEFAULT_FROM_EMAIL,
-        [to_email],
-        fail_silently=False,
+    from_email = os.environ.get("DEFAULT_FROM_EMAIL")  # your verified SendGrid email
+    activation_link = f"https://scholartrack-frontend.onrender.com/activate?token={token}"
+
+    message = Mail(
+        from_email=from_email,
+        to_emails=to_email,
+        subject="Activate Your ScholarTrack Account",
+        html_content=f"""
+        <p>Hello!</p>
+        <p>Click the link below to set your password and activate your account:</p>
+        <p><a href="{activation_link}">{activation_link}</a></p>
+        <p>If you didn't request this, ignore this email.</p>
+        """
     )
+
+    try:
+        sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+        response = sg.send(message)
+        print("Email sent with status code:", response.status_code)
+    except Exception as e:
+        print("Failed to send email:", e)
